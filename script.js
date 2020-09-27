e78517943bad1883b50315dc0aeab5847bb62259
aliadnanasim786 <aliadnanasim786@gmail.com>
2020-09-28 04:21:14 +0500

    Covid-19 Country wise Data

---
+++ script.js
@@ -0,0 +1,22 @@
+function run() {
+    let countries = document.querySelector('#inp').value;
+    let cont = document.querySelector('.container').style;
+    cont.visibility = "visible"
+
+
+    fetch(`https://corona.lmao.ninja/v2/countries/${countries}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            document.getElementById("country").innerHTML = data.country;
+            document.getElementById("active").innerHTML = data.active.toLocaleString();
+            document.getElementById("cases").innerHTML = data.cases.toLocaleString();
+            document.getElementById("critical").innerHTML = data.critical.toLocaleString();
+            document.getElementById("death").innerHTML = data.deaths.toLocaleString();
+            document.getElementById("recovered").innerHTML = data.recovered.toLocaleString();
+            document.getElementById("tests").innerHTML = data.tests.toLocaleString();
+            document.getElementById("flag").src = data.countryInfo.flag;
+        });
+}
